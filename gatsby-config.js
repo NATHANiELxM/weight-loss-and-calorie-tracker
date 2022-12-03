f40e8ed3904cce4923c3c215cb6cc1692b2fd626
Nathaniel Murphy <nathaniel_murphy5@mymail.eku.edu>
2022-12-03 02:09:34 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Weight Loss and Calorie Tracker`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
